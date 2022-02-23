9661e0522a46734bde4755d2c15f468aa9e8f13a
JeanCGuedes <jean.carlo.guedes@gmail.com>
2022-02-23 17:25:02 -0300

    first commit -Reproduz Desafio

---
+++ TODO.md
@@ -0,0 +1,8 @@
+##Cronograma:
+
+1 - Incluir Lombok;
+
+2 - Completar Classes;
+
+3 - Banco Liste Clientes e Contas;
+
---
+++ src/models/Conta.java
@@ -0,0 +1,67 @@
+package models;
+
+import interfaces.IConta;
+
+public abstract class Conta implements IConta {
+    private static final int AGENCIA_PADRAO = 1000;
+    private static int SEQUENCIAL = 1;
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+    private Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    protected void ImprimirInfosComuns() {
+        System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+        System.out.println(String.format("Agência: %d", this.getAgencia()));
+        System.out.println(String.format("Número : %d", this.getNumero()));
+        System.out.println(String.format("Saldo  : %.2f", this.getSaldo()));
+    }
+
+}
