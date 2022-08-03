35e53e788adea431cd714471632f0ff4aca5cdff
JeanCGuedes <jean.carlo.guedes@gmail.com>
2022-08-03 19:38:35 -0300

    Inclui Spring Web

---
+++ src/main/java/com/bancodio/dio/BancoDioApplication.java
@@ -0,0 +1,13 @@
+package com.bancodio.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancodio/dio/BancoDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancodio.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
